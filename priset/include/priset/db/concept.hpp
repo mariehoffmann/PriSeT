d9f06961acbe87c5cf38e6af84c08681eec3ddbd
Marie Hoffmann <marie.hoffmann@fu-berlin.de>
2018-07-23 18:38:50 +0200

    Add DB scheme for Taxonomy DB as provided on the ftp server of NCBI.

---
+++ priset/include/priset/db/concept.hpp
@@ -0,0 +1,19 @@
+// ============================================================================
+//             PriSet - Primer Search Tool for metagenomic Analyses
+// ============================================================================
+
+#pragma once
+
+#include <string>
+
+/*!\file
+ * \brief Contains concepts a database connector needs to fulfill.
+ * \author Marie Hoffmann <marie.hoffmann AT fu-berlin.de>
+ */
+
+/*
+ * A database connector provides a query builder. The query builder provides a
+ launch function to execute the query and set the fields of a result object. 
+
+
+*/
