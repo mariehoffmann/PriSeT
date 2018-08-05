ad4487a4a57d9fcde291d4bd299a8f5774464f8d
Marie Hoffmann <marie.hoffmann@fu-berlin.de>
2018-08-05 16:13:46 +0200

    Move concepts into module folders, add concepts for config and connectors.

--- priset/include/priset/database/concept.hpp
+++ priset/include/priset/database/concept.hpp
@@ -9,23 +9,43 @@
 
 #pragma once
 
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
-
-#include <priset/taxtree.hpp>
 
 namespace priset
 {
 
-/*!\addtogroup combiner
+/*!\addtogroup database
  * \{
 */
+
+//!\cond
+template <typename RefDBConfig>
+concept bool refdb_config_concept = requires (RefDBConfig c)
+{
+    typename RefDBConfig::path_type;
+    typename RefDBConfig::filename_type;
+    { c.set_basepath(RefDBConfig::path_type) } -> bool;
+    { c.get_basepath() } -> RefDBConfig::path_type;
+    { c.set_src_files(&std::vector<RefDBConfig::filename_type>) } -> bool;
+    { c.get_src_files() } -> &std::vector<RefDBConfig::filename_type>;
+};
+//!\endcond
+
+//!\cond
+template <typename RefDBConnector>
+concept bool refdb_connector = requires (RefDBConnector c)
+{
+    typename RefDBConnector::accession_type;
+    typename RefDBConnector::sequence_type;
+    { c.get_reference(RefDBConnector::accession_type) } -> RefDBConnector::sequence_type;
+};
+//!\endcond
+
 //!\cond
 template <typename DatabaseConnector>
 concept bool database_connector_concept = requires (DatabaseConnector d)
 {
-    { d.query() } -> &std::String;
+    { d.query() } -> std::String;
     { d.connection_open() } -> bool;
     { d.connection_close() } -> bool;
 };
