5f84f37e5ec27d36a425ab0c29e12cfdbf677268
Marie Hoffmann <ozymandiaz147@googlemail.com>
2019-05-17 16:04:42 +0200

    Replace priset::interval with std::pair.

--- src/primer_config.hpp
+++ src/primer_config.hpp
@@ -74,70 +74,51 @@ struct primer_config
         return root_taxid;
     }
 
-    // Set lower bound for percentage of primer-template match.
-    constexpr void set_coverage_rate(float_type coverage_rate_)
+    constexpr float get_occurence_freq() const noexcept
     {
-        assert(coverage_rate >= .5 && coverage_rate <= 1.0);
-        coverage_rate = coverage_rate_;
-    }
-
-    // Return lower bound for percentage of primer-template match.
-    constexpr float_type get_coverage_rate() const noexcept
-    {
-        return coverage_rate;
-    }
-
-    // Set mismatch_number.
-    constexpr void set_mismatch_number(size_type mismatch_number_)
-    {
-        assert(mismatch_number_ >= 0 && mismatch_number_ <= primer_length_range.first);
-        mismatch_number = mismatch_number_;
-    }
-
-    // Get mismatch_number.
-    constexpr size_type get_mismatch_number() const noexcept
-    {
-        return mismatch_number;
+        return occurrence_freq;
     }
 
     // Set melting temperature range.
-    void set_Tm_range(float_type min_Tm, float_type max_Tm)
+    void set_Tm_range(float min_Tm, float max_Tm)
     {
-        primer_melt_range = interval<float_type>{min_Tm, max_Tm};
+        primer_melt_range = std::make_pair<float, float>(float(min_Tm), float(max_Tm));
     }
 
     // Set melting temperature range.
-    constexpr interval<float_type> get_Tm_range() const noexcept
+    template<typename interval_type>
+    constexpr interval_type get_Tm_range() const noexcept
     {
         return primer_melt_range;
     }
 
     // Get minimal melting temperature.
-    constexpr float_type get_min_Tm() const noexcept
+    constexpr float get_min_Tm() const noexcept
     {
-        return primer_melt_range.min;
+        return primer_melt_range.first;
     }
 
     // Get maximal melting temperature.
-    constexpr float_type get_max_Tm() const noexcept
+    constexpr float get_max_Tm() const noexcept
     {
-        return primer_melt_range.max;
+        return primer_melt_range.second;
     }
 
     // Set molar Natrium concentration if salt adjusted method for primer Tm.
-    void set_Na(float_type Na_)
+    void set_Na(float Na_)
     {
         Na = Na_;
     }
 
     // Set molar Natrium concentration if salt adjusted method for primer Tm.
-    constexpr float_type get_Na() const noexcept
+    constexpr float get_Na() const noexcept
     {
         return Na;
     }
 
     // Get primer length range
-    constexpr interval<size_type> get_primer_length_range()
+    template<typename interval_type>
+    constexpr interval_type get_primer_length_range()
     {
         return primer_length_range;
     }
@@ -155,27 +136,31 @@ struct primer_config
     }
 
     // Set distance_range.
-    void set_transcript_range(interval<size_type> transcript_range_)
+    template<typename interval_type>
+    void set_transcript_range(interval_type transcript_range_)
     {
-        assert(transcript_range_.min >= 50 && transcript_range_.max <= 2000);
+        assert(transcript_range_.first >= 50 && transcript_range_.second <= 2000);
         transcript_range = transcript_range_;
     }
 
     // Get distance_range.
-    constexpr interval<size_type> get_transcript_range() const noexcept
+    template<typename interval_type>
+    constexpr interval_type get_transcript_range() const noexcept
     {
         return transcript_range;
     }
 
     // Get CG content range.
-    void set_CG_content_range(interval<float_type> CG_content_range_) const noexcept
+    template<typename interval_type>
+    void set_CG_content_range(interval_type CG_content_range_) const noexcept
     {
         // assert some boundaries
         CG_content_range = CG_content_range_;
     }
 
     // Get CG content range.
-    constexpr interval<float_type> get_CG_content_range() const noexcept
+    template<typename interval_type>
+    constexpr interval_type get_CG_content_range() const noexcept
     {
         return CG_content_range;
     }
@@ -185,34 +170,31 @@ private:
     // Root taxonomic identifier below which references are sampled.
     taxid_type root_taxid{1};
 
-    // Lower bound for coverage rate of primer to template alignment.
-    float_type coverage_rate{.8};
+    // Occurrence frequency of primer sequences relative to the number of taxa with at least one existing reference sequence.
+    float occurrence_freq{0.5};
 
-    // Upper bound for number of mismatches between primer and template.
-    size_type mismatch_number{3};
-
-    // Primer length range.
-    interval<size_type> primer_length_range{interval<size_type>{18, 24}};
+    // Primer length rang
+    std::pair<size_type, size_type> primer_length_range{18, 24};
 
     // Transcript length range.
-    interval<size_type> transcript_range{interval<size_type>{30, 700}};
+    std::pair<size_type, size_type> transcript_range{30, 700};
 
     // Range of primer melting temperatures (best results in range [52-58] degree C).
-    interval<float_type> primer_melt_range{52, 58};
+    std::pair<float, float> primer_melt_range{52.0, 58.0};
 
     // Maximal permitted temperature difference [Kelvin] of primers.
     // Differences above 5 Kelvin can lead to no amplification.
-    float_type primer_melt_diff{4.0};
+    float primer_melt_diff{4.0};
 
     // Method for computing melting temperature of primer.
     chemistry::method primer_melt_method{chemistry::method::wallace};
 
     // Molar Natrium concentration for salt-adjusted primer_melt_method.
-    float_type Na{.0};
+    float Na{.0};
 
     // CG content range of primer.
     // It is recommended that the relative content of nts C and G is in the range of 40%-60%.
-    interval<float_type> CG_content_range{.4, .6};
+    std::pair<float, float> CG_content_range{.4, .6};
 };
 
 }  // namespace priset
